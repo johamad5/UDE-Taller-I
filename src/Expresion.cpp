2143da0d388d85034c5c1936f7bb867c916690ad
Matias <matiasmirenda@gmail.com>
2026-02-24 20:17:12 -0300

    fix test run and add todo empty function

--- src/Expresion.cpp
+++ src/Expresion.cpp
@@ -1,8 +1,14 @@
 #include "Expresion.h"
 
-int ObtenerIndiceExpresion(Expresion exp) {}
+int ObtenerIndiceExpresion(Expresion exp)
+{
+    // TODO: implementar
+    return 0;
+}
 
-void MostrarExpresion(Expresion exp) {}
+void MostrarExpresion(Expresion exp)
+{
+}
 
 void DestruirExpresion(Expresion &exp)
 {

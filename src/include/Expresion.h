0598e26d260a72ca29b87e7d5a0f764ced0e8c83
Matias <matiasmirenda@gmail.com>
2026-02-24 18:11:33 -0300

    separamos expresion de expresionABB

--- src/include/Expresion.h
+++ src/include/Expresion.h
@@ -1,51 +1,20 @@
 #ifndef EXPRESION_H_INCLUDED
 #define EXPRESION_H_INCLUDED
-#include "TipoDato.h"
-#include "Operacion.h"
-#include "Boolean.h"
+#include "ExpresionABB.h"
 
-typedef struct nodoA
+typedef struct
 {
-    TipoDato tipo;
-    int indiceNodo;
-    union
-    {
-        int num;
-        char simbolo;
-    } dato;
-    nodoA *hizq;
-    nodoA *hder;
-} NodoABB;
-typedef NodoABB *ExpresionABB;
+    ExpresionABB terminos;
+    int indiceLista;
+} Expresion;
 
-// Reserva memoria para un nodo ABB e inicializa sus enlaces.
-void CrearExpresionSimpleX(ExpresionABB &n, char c);
+// Devuelve el indice de una expresion dada
+int ObtenerIndiceExpresion(Expresion exp);
 
-// Reserva memoria para un nodo ABB e inicializa sus enlaces.
-void CrearExpresionSimpleInt(ExpresionABB &n, int num);
+// Muestra por pantalla la expresion
+void MostrarExpresion(Expresion exp);
 
-// Devuelve el índice máximo presente en el ABB.
-int indiceMaximoABB(ExpresionABB abb);
-
-// Indica si dos árboles son idénticos en estructura y contenido.
-Boolean ArbolesIdenticos(ExpresionABB a, ExpresionABB b);
-
-// Calcula el resultado del ABB evaluando la variable x.
-void CalcularABB(ExpresionABB nodo, int &valorX, Boolean &errorDivision);
-
-// Llibera toda la memoria del ABB.
-void DestruirABB(ExpresionABB &abb);
-
-// Ajusta los índices del árbol
-void ajustarIndicesABB(ExpresionABB &abb, int delta);
-
-// Copia un árbol completo.
-ExpresionABB copiarArbol(ExpresionABB original);
-
-// Ingresa un paréntesis izquierdo como último hizq.
-void insertarParentesisIzquierdo(ExpresionABB &abb);
-
-// Ingresa un paréntesis derecho como último hder.
-void insertarParentesisDerecho(ExpresionABB &abb);
+// Destruye la expresion y libera toda la memoria
+void DestruirExpresion(Expresion &exp);
 
 #endif

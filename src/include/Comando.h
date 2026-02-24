4bac559f776cba794cc53f3c8b887f8ba823b458
Johana Madero <johanamadero3@gmail.com>
2026-02-24 02:33:07 -0300

    Comando module OK. Switch to uppercase for easy checking

--- src/include/Comando.h
+++ src/include/Comando.h
@@ -1,6 +1,15 @@
 #ifndef COMANDO_H_INCLUDED
 #define COMANDO_H_INCLUDED
 
-typedef enum { Simple, Compuesta, Calcular, Mostrar, Recuperar, Guardar, Salir } TipoComando;
+typedef enum
+{
+    SIMPLE,
+    COMPUESTA,
+    CALCULAR,
+    MOSTRAR,
+    RECUPERAR,
+    GARDAR,
+    SALIR
+} TipoComando;
 
 #endif
